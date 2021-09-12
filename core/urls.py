f8170bb8194eeb47a3e836d873700db5e32e6c9c
Andre Carvalho <alac1984@gmail.com>
2021-09-12 17:23:32 -0300

    Basic configuration finished

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index')
+    path('', views.index_view, name='index'),
+    path('search/<str:query>', views.search_view, name='search'),
+    path('search/<str:query>/<int:pk>', views.detail_view, name='detail')
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,10 @@ def index_view(request):
     return render(request, 'core/index.html')
 
 
+def search_view(request, query):
+    return render(request, 'core/search.html', {'query': query})
+
+
+def detail_view(request, query, pk):
+    return render(request, 'core/detail.html', {'pk': pk})
+
